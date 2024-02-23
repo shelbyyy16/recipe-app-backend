a858e579cf3ac4260144fbae3ab25f803f1667e1
honeybee <shelby.e.pagan@gmail.com>
2024-02-23 14:29:58 -0500

    initial express build out. create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+// Require modules
+const express = require('express');
+  
+// Create the Express app
+const app = express();
+  
+// Configure the app (app.set)
+  
+  
+// Mount middleware (app.use)
+  
+  
+// Mount routes
+app.get('/', function(req, res, next) {
+    res.send('<h1>Hello Express</h1>');
+  });
+
+  
+// Tell the app to listen on port 3000
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
