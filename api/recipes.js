9931880e25342c1b893b1ea6d9a88a5d1e647850
honeybee <shelby.e.pagan@gmail.com>
2024-02-28 12:13:55 -0500

    update route for api/recipes

--- api/recipes.js
+++ api/recipes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Recipe = require('../models/Recipe'); 
 
 // Route to get all recipes
-router.get('/', async (req, res) => {
+router.get('/api/recipes', async (req, res) => {
   try {
     const recipes = await Recipe.find();
     res.json(recipes);
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
 });
 
 // Route to add a new recipe
-router.post('/', async (req, res) => {
+router.post('/api/recipes', async (req, res) => {
   const { name, ingredients, instructions } = req.body;
 
   try {
@@ -26,7 +26,6 @@ router.post('/', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
 
 // Other routes for updating and deleting recipes can be added as needed
 
